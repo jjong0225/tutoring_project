5d0bde3f50cf6e9fd246b300c11151f947864a44
jjong0225 <jjong0225@naver.com>
2021-02-19 20:33:34 +0900

    template

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 #include<string>
-
+#include "objects.h"
+#include "data_structure.h"
+#include "file_read.h"
 using namespace std;
+
+const int NUM_OF_STATION = 100;
+Node STATION_NODE[NUM_OF_STATION] = {0};
+
+
 int main()
 {
+    // 파일 읽는 부분
+    read_user_data();
+    read_metro_data();
     while(1)
     {
         string command;
