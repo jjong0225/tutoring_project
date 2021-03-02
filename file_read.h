612df81f64a7cff8ac450f18c0507505c20a3943
sjoonb <sungjun9808@gmail.com>
2021-03-03 01:27:18 +0900

    deleted unnecessary files

--- file_read.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_READ_H
-#define FILE_READ_H
-
-#include "data_structure.h"
-UserTreeNode* read_user_data();
-MetroTreeNode* read_metro_data();
-void save_user_data(UserTreeNode* root);
-void save_metro_data(MetroTreeNode* root);
-#endif
