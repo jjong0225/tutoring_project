4abbb8029140a3ed50f7e1d6a1efbe54cb16d7ea
sjoonb <sungjun9808@gmail.com>
2021-03-14 10:07:48 +0900

    solved infinite loop error

--- main.cpp
+++ main.cpp
@@ -19,29 +19,6 @@ int main()
     UserTreeNode* user_root = read_user_data();
     MetroTreeNode* metro_root = read_metro_data();
 
-//    printf("------user nodes-------\n");
-//    print_node(user_root);
-//    printf("----------metro nodes---------\n");
-//    print_node(metro_root);
-
-//	save_user_data(user_root);
-//	save_metro_data(metro_root);
-//
-    
-    //Algorithm 
-   
-    //init_graph(metro_root);
-    //tuple<string,Track_info,Track_info> schedule_result= find_optimized_schedule_path(-152022995,248006855,1100,1500);
-    //현재 station id, 다음 스케줄 station id, 현재 시간 cur time , 다음 스케줄 시작 시간 nex time
-    //if(get<0>(schedule_result)!=" ") {
-   // printf("----------------Optimize Schedules---------------\n\n");    
-    //cout<<get<0>(schedule_result)<<endl;
-    //printf("----------Mininmum estimated arrival time Path----------\n");
-    //search_optimize_schedule(get<1>(schedule_result).path_list,get<1>(schedule_result).departure_time); // 최소 소요 시간 track
-    //printf("\n-------------Maximum departure time Path-------------\n");
-    //search_optimize_schedule(get<2>(schedule_result).path_list,get<2>(schedule_result).departure_time); // 최대 출발 track
-    //} 
-
 
     while(command != 3)
     {
@@ -54,7 +31,16 @@ int main()
         cout<< "1. 유저 데이터 관리"<< endl;
         cout<< "2. 스케줄 관리"<< endl;
         cout<< "3. 프로그램 종료"<< endl;
-        cin >> command;
+
+		cin >> command;
+
+		cin.clear(); //failbit
+		cin.ignore(10, '\n'); //
+
+		cout << command;
+
+
+	
 
     
         switch (command)
@@ -240,6 +226,7 @@ int main()
                 	}
                 
             	}
+				break;
 			}
 			
             case 3: {
@@ -248,7 +235,6 @@ int main()
                     cout << "변경사항을 저장하시겠습니까? 1. 저장 2. 저장하지않음"<< endl;
                     cin>> savec;
                     if(savec == 1){
-                        save_metro_data(metro_root);
                         save_user_data(user_root);
                         savecheck = 0;
                         cout << "저장했습니다"<< endl;
@@ -263,7 +249,7 @@ int main()
 					cout << "번호를 잘못입력하셨습니다. 다시 입력해주세요!"<<endl;
 					break;
 				}
-		
+
 
 		}
     }
