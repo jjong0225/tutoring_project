db349d860444cd67b6b5c53dfabe1933043f242c
hyom <qet557@gmail.com>
2021-03-17 04:20:16 +0900

    match schedule fuc added

--- algorithm.h
+++ algorithm.h
@@ -26,6 +26,8 @@ int min_to_hour(int time);
 int cal_time(int input_time);
 int hour_to_min(int time);
 string print_HHMM(string  val );
+bool Metro_check(MetroTreeNode * root_ptr, int id);
+int convert_time_input(int input);
 pair<string, vector<pair<int,int>>> track_path(int departure_id,int arrive_id, map<int ,tuple<int,int,int>> path);
 pair<int,int> find_min_path(int present_station,int next_station,int start_time,bool op);
 pair<map<int,int>, map <int,tuple<int,int,int>>> trans_dijkstra(int departure_id ,int arrive_id,int time);
@@ -33,6 +35,7 @@ int transfer_num(vector<pair<int,int>>Track);
 void search_optimize_schedule(vector<pair<int,int>> search_line, int departure_time);
 int find_fair_station(map<int,int>a, map<int,int>b);
 pair<int, pair<string,vector<pair<int,int>>>>find_max_departure_time(int A_station_id , int B_station_id ,int now_time,int deadline_time);
+bool match_schedule(MetroTreeNode *metro_r, UserTreeNode * user_r, UserTreeNode *A,UserTreeNode *B, int meeting_start, int meeting_end, int now_time, string name);
 tuple<string,Track_info,Track_info> find_optimized_schedule_path(int A_station, int B_station,int end_schedule_time, int next_schedule_time);
 
 #endif
